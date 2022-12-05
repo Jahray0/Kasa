316ac676719ccf8d54b1955f83a593e904c45f1b
Jahray0 <jaegle.maxime@gmail.com>
2022-12-05 18:31:39 +0100

    v1.006 changer footer

--- src/pages/Page404/Page404.jsx
+++ src/pages/Page404/Page404.jsx
@@ -1,7 +1,18 @@
-//import styles from "./Title.module.scss";
+import { Link } from "react-router-dom";
+import "../../styles/Page404.css";
 
 function Page404() {
-  return <p>coucou</p>;
+  return (
+    <div className="error__container">
+      <p className="error__number">404</p>
+      <p className="error__text">
+        Oups ! La page que vous demandez n'existe pas
+      </p>
+      <Link to="/" className="link__home">
+        Retourner sur la page d'accueil
+      </Link>
+    </div>
+  );
 }
 
 export default Page404;
