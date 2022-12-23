11e4d5af20b3170a4679acec645530e2d2e53ac3
Jahray0 <jaegle.maxime@gmail.com>
2022-12-23 16:00:26 +0100

    ppt 3

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -4,7 +4,7 @@ import logo_footer from "../assets/logo_footer.png";
 function Footer() {
   return (
     <footer>
-      <div className="f_container">
+      <div className="footer_container">
         <div className="column">
           <img src={logo_footer} alt="coucou" />
           <p className="right">© 2020 Kasa. All rights reserved</p>
